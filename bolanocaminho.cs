b9b5ab98ab7a95441d7e5b03615d9e8758792fe2
adamascj2 <adamascj@gmail.com>
2020-09-26 18:26:12 -0300

    Add files via upload

---
+++ bolanocaminho.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bolanocaminho : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     iTween.MoveTo(this.gameObject,iTween.Hash("path",iTweenPath.GetPath("caminhobola"),"time",5,"looptype","loop","orienttopath",true,"looktime",1,"easeType",iTween.EaseType.linear));   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Pertobola2.perto) iTween.Stop(this.gameObject);
+    }
+}
