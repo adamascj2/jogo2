888ec8727bcac5de2df07f82b5370d70c77e6517
adamascj2 <adamascj@gmail.com>
2020-09-26 16:06:03 -0300

    Add files via upload

---
+++ Empurra.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+ 
+public class Empurra: MonoBehaviourPun
+{
+   float grandeEmpurro = 2.0f;
+   Vector3 direcaoEmpurro; 
+ void OnControllerColliderHit(ControllerColliderHit hit){
+   Rigidbody bodyEmpurrado = hit.collider.attachedRigidbody;
+   if(bodyEmpurrado ==null || bodyEmpurrado.isKinematic)
+    return;
+   if(hit.moveDirection.y <-0.3f)
+    return;
+    direcaoEmpurro = new Vector3 (hit.moveDirection.x,0,hit.moveDirection.z);
+   bodyEmpurrado.velocity = direcaoEmpurro*grandeEmpurro;
+ }
+   
+  
+}
