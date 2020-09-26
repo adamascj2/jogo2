daee51309d8ce01839d975916dfdd0148f007f9a
adamascj2 <adamascj@gmail.com>
2020-09-26 18:26:40 -0300

    Add files via upload

---
+++ Pertobola2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pertobola2 : MonoBehaviour
+{
+   
+   
+   public static bool perto =false;
+   
+     void Update()
+    {
+     GameObject GOB = GameObject.Find("boladefogoa");
+       if(Vector3.Distance(transform.position,GOB.transform.position)<10 && GOB!=null){
+          perto=true; 
+          GOB.transform.rotation = Quaternion.Slerp(GOB.transform.rotation, Quaternion.LookRotation(transform.position-GOB.transform.position),2*Time.deltaTime);
+          GOB.transform.Translate(0,0,4*Time.deltaTime); 
+          
+        }
+       
+       
+    } 
+    
+}
+
